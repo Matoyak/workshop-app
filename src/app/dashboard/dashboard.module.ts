270f1cf5fd4bc596e05ff1219d6d1b106790e81f
Matoyak <mwilliam09@gmail.com>
2020-09-30 14:23:58 -0500

    Added VideoLoaderService

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+
 import { VideoDashboardComponent } from './video-dashboard/video-dashboard.component';
 import { VideoListComponent } from './video-list/video-list.component';
 import { VideoPlayerComponent } from './video-player/video-player.component';
@@ -7,7 +9,6 @@ import { StatFiltersComponent } from './stat-filters/stat-filters.component';
 import { RouterModule } from '@angular/router';
 import { VideoDetailsComponent } from './video-details/video-details.component';
 import { VideoCardComponent } from './video-card/video-card.component';
-import { HttpClientModule } from '@angular/common/http';
 
 const dashboardRoutes = [
 	{ path: '', pathMatch: 'full', redirectTo: 'videoDashboard' },
--- src/app/services/video-loader.service.ts
+++ src/app/services/video-loader.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { map, tap } from 'rxjs/operators';
 
 import { environment } from 'src/environments/environment';
 import { Video } from '../models';
@@ -12,6 +13,10 @@ export class VideoLoaderService {
 	constructor(private http: HttpClient) {}
 
 	loadVideos(): Observable<Video[]> {
-		return this.http.get<Video[]>(environment.apiUrl + '/videos');
+		return this.http.get<Video[]>(environment.apiUrl + '/videos').pipe(
+			tap({ next: (videos) => console.log('before map', videos) }),
+			map((videos) => videos.slice(0, 2)),
+			tap((videos) => console.log('after map', videos))
+		);
 	}
 }
